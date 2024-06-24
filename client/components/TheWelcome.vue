7ebe0b17681e6661552bce3f088bfa87ca4772ba
Sean Kennedy <sean@codeserious.com>
2024-06-24 15:34:41 -0400

    Adjust routes, add global header and footer

--- client/components/TheWelcome.vue
+++
@@ -1,6 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1>Welcome</h1>
-  <h1><a href="/about">Template page</a></h1>
-</template>
--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <select>
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+    </select>
+
+    <button type="submit">Submit</button>
+  </div>
 </template>
