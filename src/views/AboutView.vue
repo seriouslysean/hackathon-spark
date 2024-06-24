3ce9eff7d40b5fe7f2f96d88801efac491bf0ad0
Jack Keller <j3kk3l@gmail.com>
2024-06-24 12:12:36 -0400

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,74 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<template class="about">
+    <div class="header-section">
+        <h3>Dates</h3>
+        <h4>Web</h4>
+        <p>Nov 1, 2022</p>
+        <h4>Mobile</h4>
+        <p>Nov 2, 2022</p>
+
+    </div>
+
+  <div class="header-section">
+      <h3>Web Versions</h3>
+      <p><a href="#">Jira Tickets</a></p>
+
+      <ul>
+          <li>SlipStream: 3.77</li>
+          <li>MonoWeb: 2.109</li>
+      </ul>
   </div>
+
+    <div class="team">
+        <h4>Expansion</h4>
+
+        <h5>Web</h5>
+
+        <ul>
+            <li>Added</li>
+            <ul>
+              <li>None</li>
+            </ul>
+            <li>Fixed</li>
+            <ul>
+              <li>Content not displaying on tablet</li>
+            </ul>
+        </ul>
+
+        <h5>Mobile</h5>
+
+        <ul>
+            <li>Added</li>
+            <ul>
+                <li>None</li>
+            </ul>
+            <li>Fixed</li>
+            <ul>
+                <li>None</li>
+            </ul>
+        </ul>
+    </div>
+
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
+.about {
     min-height: 100vh;
+    max-width: 800px;
+    padding: 30px;
     display: flex;
-    align-items: center;
-  }
+
+}
+
+.header-section {
+    width: 400px;
+    margin: 20px;
+    padding: 20px;
+    border: 1px dashed #CCC;
+}
+
+h3 {
+    background-color: #bada55;
+    padding: 5px;
+    margin-bottom: 10px;
 }
 </style>
