6f92a3fce4cf3df01d4c74f0f6a42a557348d301
Jack Keller <j3kk3l@gmail.com>
2024-06-24 14:11:00 -0400

    more better styling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,24 @@
-<template class="about">
-    <div class="header-section">
-        <h3>Dates</h3>
-        <h4>Web</h4>
-        <p>Nov 1, 2022</p>
-        <h4>Mobile</h4>
-        <p>Nov 2, 2022</p>
+<template>
+    <div class="headers">
+        <div class="header-section">
+            <h3>Dates</h3>
+            <h4>Web</h4>
+            <p>Nov 1, 2022</p>
+            <h4>Mobile</h4>
+            <p>Nov 2, 2022</p>
 
-    </div>
+        </div>
 
-  <div class="header-section">
-      <h3>Web Versions</h3>
-      <p><a href="#">Jira Tickets</a></p>
+      <div class="header-section">
+          <h3>Web Versions</h3>
+          <p><a href="#">Jira Tickets</a></p>
 
-      <ul>
-          <li>SlipStream: 3.77</li>
-          <li>MonoWeb: 2.109</li>
-      </ul>
-  </div>
+          <ul>
+              <li>SlipStream: 3.77</li>
+              <li>MonoWeb: 2.109</li>
+          </ul>
+      </div>
+     </div>
 
     <div class="team">
         <h4>Expansion</h4>
@@ -48,20 +50,47 @@
         </ul>
     </div>
 
+    <div class="team">
+        <h4>Explore and Evaluation</h4>
+
+        <h5>Web</h5>
+
+        <ul>
+            <li>Added</li>
+            <ul>
+              <li>Quick add bundling section on PDP</li>
+            </ul>
+            <li>Fixed</li>
+            <ul>
+              <li>Country Param being added as a refinement</li>
+              <li>Jump links are broken</li>
+            </ul>
+        </ul>
+
+        <h5>Mobile</h5>
+
+        <ul>
+            <li>Added</li>
+            <ul>
+                <li>Quick Add bundling section on PDP</li>
+            </ul>
+            <li>Fixed</li>
+            <ul>
+                <li>FP Classes navigates user to ShopTab prior to showing content</li>
+                <li>1 Android crasher resolved</li>
+            </ul>
+        </ul>
+    </div>
+
 </template>
 
 <style>
-.about {
-    min-height: 100vh;
-    max-width: 800px;
-    padding: 30px;
-    display: flex;
-
+.headers {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 10px;
 }
-
 .header-section {
-    width: 400px;
-    margin: 20px;
     padding: 20px;
     border: 1px dashed #CCC;
 }
@@ -71,4 +100,13 @@ h3 {
     padding: 5px;
     margin-bottom: 10px;
 }
+
+h4 {
+    font-size: 22px;
+}
+
+h5 {
+    font-weight: bold;
+}
+
 </style>
