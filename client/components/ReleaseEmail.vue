0753eb9fdd05c36d881d92ee08c2bee6a2e39d40
Jack Keller <j3kk3l@gmail.com>
2024-06-25 13:26:21 -0400

    styling

--- client/components/ReleaseEmail.vue
+++ client/components/ReleaseEmail.vue
@@ -40,7 +40,7 @@ try {
             <p>Failed to fetch the release.</p>
         </div>
         <div v-else class="email">
-            <div class="headers">
+            <!-- <div class="headers">
                 <div class="header-section">
                     <h3>Dates</h3>
                     <h4>Web</h4>
@@ -55,22 +55,49 @@ try {
                         <li>Version {{ releaseData.title }}</li>
                     </ul>
                 </div>
+            </div> -->
+
+            <div class="section">
+                <p>Hello,</p>
+                <p>Here are the latest release notes for {{ releaseData.title }}:</p>
             </div>
 
-            <div v-for="(team, index) in releaseData.teams" :key="index" class="team">
-                <h4>{{ team.name }}</h4>
-                <h5>Customer Facing</h5>
+            <div class="section">
+
+                <h3>Feature Releases and Highlights</h3>
                 <ul>
-                    <li v-for="ticket in team.tickets.filter(t => t.customerFacing)" :key="ticket.ticket">
-                        {{ ticket.title }} - {{ ticket.summary }}
-                    </li>
+                    <li>highlight 1</li>
+                    <li>highlight 2</li>
+                    <li>highlight 3</li>
                 </ul>
-                <h5>Not Customer Facing</h5>
-                <ul>
-                    <li v-for="ticket in team.tickets.filter(t => !t.customerFacing)" :key="ticket.ticket">
-                        {{ ticket.title }} - {{ ticket.summary }}
-                    </li>
-                </ul>
+
+            </div>
+
+            <div class="section">
+
+                <h3>Bug Fixes and Improvements</h3>
+
+                <div v-for="(team, index) in releaseData.teams" :key="index" class="team">
+                    <h4>{{ team.name }}</h4>
+                    <h5>Customer Facing</h5>
+                    <ul>
+                        <li v-for="ticket in team.tickets.filter(t => t.customerFacing)" :key="ticket.ticket">
+                            {{ ticket.title }} - {{ ticket.summary }}
+                        </li>
+                    </ul>
+                    <h5>Not Customer Facing</h5>
+                    <ul>
+                        <li v-for="ticket in team.tickets.filter(t => !t.customerFacing)" :key="ticket.ticket">
+                            {{ ticket.title }} - {{ ticket.summary }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="section">
+                <p>For more details on each line item, please refer to <a href="#">all JIRA tickets in this release</a></p>
+
+                <p>Thank you!</p>
             </div>
         </div>
     </div>
--- client/views/ReleaseView.vue
+++ client/views/ReleaseView.vue
@@ -28,7 +28,7 @@ const selectedRelease = computed(() => releaseStore.selectedRelease);
 </template>
 
 <style>
-.headers {
+/* .headers {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-column-gap: 10px;
@@ -37,7 +37,7 @@ const selectedRelease = computed(() => releaseStore.selectedRelease);
 .header-section {
   padding: 20px;
   border: 1px dashed #ccc;
-}
+} */
 
 .team {
   border-bottom: 1px solid #bbb;
@@ -45,10 +45,13 @@ const selectedRelease = computed(() => releaseStore.selectedRelease);
   padding-bottom: 20px;
 }
 
+.section {
+    padding: 20px 0;
+}
+
 h3 {
-  background-color: #bada55;
-  padding: 5px;
-  margin-bottom: 10px;
+  font-size: 20px;
+  font-weight: bold;
 }
 
 h4 {
@@ -57,5 +60,9 @@ h4 {
 
 h5 {
   font-weight: bold;
+}
+
+ul {
+    padding-left: 30px;
 }
 </style>
